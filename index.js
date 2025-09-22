2e56afaeceb4d1710fb599ce7d0de953869f3ac0
Nikhil Bhardwaj <nikhillbhardwajjj@gmail.com>
2025-09-22 21:43:17 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('hello devops' )
+    console.log('i am learning CI/CD using github actions')
+    
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('start tests...' ) 
+setTimeout(()=> console.log('waiting 4 seconds...' ), 4000) ; 
+console.log('starting tests...' );
